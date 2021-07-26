108a72551b4c2194616a4fc67a5636d3d418dba8
Mamunur Rashid Mukto <34998258+mukto33@users.noreply.github.com>
2021-07-26 22:13:15 +0600

    Just from Netbeans

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,16 @@
+
+package javaapplication1;
+
+/**
+ *
+ * @author mamun
+ */
+public class JavaApplication1 {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Mukto");
+    }
+            
+            
+}
