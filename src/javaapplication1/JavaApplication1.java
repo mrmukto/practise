03eb336b429c4145d0f2188a8a21266b9018839c
Mamunur Rashid Mukto <34998258+mukto33@users.noreply.github.com>
2021-07-26 22:39:00 +0600

    Just from Netbeans new

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -10,6 +10,8 @@ public class JavaApplication1 {
  
     public static void main(String[] args) {
         System.out.println("Mukto");
+        
+        System.out.println("Again some change");
     }
             
             
